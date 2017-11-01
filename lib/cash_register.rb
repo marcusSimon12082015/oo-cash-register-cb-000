84480e04ec3935724de2628624ce4122a69107b2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 16:45:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,12 @@
 class CashRegister
   attr_accessor :total, :discount
   @@items = []
+
   def initialize(discount = 0)
       @total = 0
       @discount = discount
   end
+
   def add_item(title,price,quantity = 0)
     if quantity == 0
       @total +=price
